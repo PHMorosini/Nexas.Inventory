27cecadd695c4c8c77ccc6c57924ff641e086dad
Pedro Henrique Alves Morosini <pedro.morosini@objetivasolucoes.com.br>
2025-08-12 08:28:26 -0300

    Feat -> Implemeted JWT settings and dependecies

---
+++ Nexas.Inventory/Application/Auth/Interface/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿using Nexas.Inventory.Domain.User.Entity;
+
+namespace Nexas.Inventory.Application.Auth.Interface;
+public interface ITokenService
+{
+     Task<string> GenerateToken(UserEntity user);
+}
---
+++ Nexas.Inventory/Application/Auth/ViewModel/AuthResponseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Nexas.Inventory.Application.Auth.ViewModel;
+public class AuthResponseViewModel
+{
+    public string Token { get; set; }
+    public DateTime Expiration { get; set; }
+    public AuthUserViewModel User { get; set; }
+}
+
